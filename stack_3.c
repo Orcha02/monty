c6e2740689f48ed9135257c41e1b7a2c070612ec
Orcha02 <orcha2002@gmail.com>
2021-05-14 01:20:07 +0000

    Fix error message of mul

--- stack_3.c
+++ stack_3.c
@@ -65,7 +65,7 @@ void _mul(stack_t **top, unsigned int line_number)
 
 	if (*top == NULL || (*top)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	tmp = (*top)->next;
